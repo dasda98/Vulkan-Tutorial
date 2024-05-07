47e2282f9c1231de2832df015c2020dc543f82da
dasda98 <wojniakjakub@gmail.com>
2024-05-08 01:02:29 +0200

    Add validation to Validation

--- Validation/validation.c
+++ Validation/validation.c
@@ -1,11 +1,17 @@
 #include "validation.h"
 
+#include <stdio.h>
+
+#include "../helpers/helpers.h"
+
 #include <string.h>
+#include <stdlib.h>
 
 
 const char* validationLayers[] = {
-    "VK_LAYER_KHRONOS_validation"
+    "VK_LAYER_KHRONOS_validation",
 };
+uint32_t vlSize = sizeof(validationLayers) / sizeof(validationLayers[0]);
 
 #ifdef NDEBUG
     const bool enableValidationLayers = false;
@@ -23,12 +29,11 @@ bool checkValidationLayerSupport() {
     size_t validationLayersSize = sizeof(validationLayers)/sizeof(char *);
     size_t availableLayersSize = sizeof(availableLayers)/sizeof(availableLayers[0]);
 
-    int i, j;
-    for (i=0; i<validationLayersSize; i++) {
+    for (int i=0; i<validationLayersSize; i++) {
         const char* layerName = validationLayers[i];
         bool layerFound = false;
 
-        for (j=0; j<availableLayersSize; j++) {
+        for (int j=0; j<availableLayersSize; j++) {
             if (strcmp(layerName, availableLayers[j].layerName) == 0) {
                 layerFound = true;
                 break;
@@ -40,3 +45,95 @@ bool checkValidationLayerSupport() {
     }
     return true;
 }
+
+const char** getRequiredExtensions(uint32_t* extensionCount) {
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(extensionCount);
+
+    const char** extensions = malloc((*extensionCount) * sizeof(char*));
+
+    if (extensions == NULL) {
+        return NULL;
+    }
+
+    memcpy(extensions, glfwExtensions, (*extensionCount) * sizeof(char*));
+
+    if (enableValidationLayers) {
+        addElement(&extensions, extensionCount, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        addElement(&extensions, extensionCount, "VK_KHR_portability_enumeration");
+    }
+
+    return extensions;
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+
+    fprintf(stderr, "Validation layer: %s\n", pCallbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT* createInfo) {
+
+    createInfo->sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo->messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | \
+                                 VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | \
+                                 VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo->messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | \
+                             VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | \
+                             VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo->pfnUserCallback = debugCallback;
+    createInfo->pNext = NULL;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger) {
+
+    PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(
+        instance,
+        "vkCreateDebugUtilsMessengerEXT");
+
+    if (func != NULL) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT debugMessenger,
+    const VkAllocationCallbacks* pAllocator) {
+
+    PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(
+        instance,
+        "vkDestroyDebugUtilsMessengerEXT");
+
+    if (func != NULL) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Validation/validation.h
+++ Validation/validation.h
@@ -7,8 +7,21 @@
 #include <stdbool.h>
 
 extern const char* validationLayers[];
+extern uint32_t vlSize;
 extern const bool enableValidationLayers;
 
 bool checkValidationLayerSupport();
+const char** getRequiredExtensions();
+
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT* createInfo);
+void DestroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT debugMessenger,
+    const VkAllocationCallbacks* pAllocator);
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger);
 
 #endif //VALIDATION_H
